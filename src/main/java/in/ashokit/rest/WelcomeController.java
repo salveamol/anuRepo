61ab6b32ef8bc13f3febed6560d740fbd06ddc78
unknown <salveamol462@gmail.com>
2022-08-05 12:28:01 +0530

    my app

---
+++ src/main/java/in/ashokit/rest/WelcomeController.java
@@ -0,0 +1,15 @@
+package in.ashokit.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/")
+  public String welcome()
+  {
+	  return "Welcome to ashok It..";
+  }
+
+}
